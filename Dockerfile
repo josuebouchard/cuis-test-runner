f4c7e410aeae909fe4d903f53657dc2d0cd471ea
josuebouchard <7890663+josuebouchard@users.noreply.github.com>
2023-04-07 11:22:15 -0300

    python version

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,9 @@
-FROM golang:1.20.3-alpine AS builder
-
-COPY main.go .
-RUN go build -o /main ./main.go
-
-FROM debian:11.6-slim
+FROM python:3.11-slim-bullseye
 
 # Used to run smalltalk ui:
 # RUN apt-get update && \
 #       apt-get -y install libxrender-dev libgl-dev libsm-dev libpulse-dev
 
 ADD ./linux64/ /home/linux64/
-COPY runTests.st /home/
-COPY --from=builder /main /home
-ENTRYPOINT [ "/home/main" ]
+COPY runTests.st main.py /home/
+ENTRYPOINT [ "python", "/home/main.py" ]
---
+++ main.py
@@ -0,0 +1,59 @@
+import os
+import sys
+from pathlib import Path
+import subprocess
+
+def parse_tests_output(tests_output):
+    parsed_output = {}
+    for part in tests_output.split(","):
+        elements = part.split(" ")[::-1]
+        elements[1] = int(elements[1])
+
+        parsed_output[elements[0]] = elements[1]
+    return parsed_output
+
+def main():
+    workspace = Path(os.environ.get("GITHUB_WORKSPACE", ""))
+
+    if len(sys.argv) < 2 + 1:
+        print("Not enough arguments")
+        exit(2)
+
+    filepaths = sys.argv[1]
+    test_classes = sys.argv[2]
+
+    cuis_params = [
+        "-vm-display-null", "-vm-sound-null",
+        "/home/linux64/CuisUniversity-5706.image", "-e",
+        "-d", "Utilities setAuthorName: 'algo3' initials: 'algo3'",
+    ]
+
+    for filepath in filepaths.split(","):
+        filepath = Path(filepath)
+        file = workspace / filepath
+
+        if not file.is_file():
+            print("No se pudo encontrar el archivo", str(file))
+            exit(2)
+
+        cuis_params.extend(['-l', str(file.absolute())])
+
+    cuis_params.extend(["-s", "/home/runTests.st"])
+    cuis_params.extend(test_classes.split(","))
+
+    cmd = subprocess.run([
+        "/home/linux64/vm-jit/squeak",
+        *cuis_params
+    ], capture_output=True)
+
+    output = cmd.stdout.decode("utf-8").strip()
+    parsed_output = parse_tests_output(output)
+
+    print(parsed_output)
+
+    if parsed_output['failed'] > 0 or parsed_output['errors'] > 0:
+        print("There are failed or errored tests")
+        exit(1)
+
+if __name__ == "__main__":
+    main()
